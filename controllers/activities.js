9c0d082389f77062e68640713f15a0b8985d220c
marramags <marra.emata@gmail.com>
2022-08-29 15:07:21 -0700

    fixed acitivities Index bug and added newActivity to routes & controller

--- controllers/activities.js
+++ controllers/activities.js
@@ -2,11 +2,16 @@ const Activity = require('../models/activity');
 
 function activitiesIndex (req, res){
     Activity.find({}, function(err, activities){
-        if (err) return res.redirect('/');
-        res.render('activities/index', {title: 'All Activities', activities});
+        // if (err) return res.redirect('/');
+        res.render('activities/index', { title: 'All Activities', activities });
     })
 }
 
+function newActivity (req, res) {
+    res.render('activities/new');
+}
+
 module.exports = {
-    activitiesIndex
+    activitiesIndex,
+    newActivity
 }
--- routes/activities.js
+++ routes/activities.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 const activitiesCtrl = require('../controllers/activities');
 
+// http://localhost:3000/activities
 router.get('/', activitiesCtrl.activitiesIndex);
+
+//http://localhost:3000/activities/new
+router.post('/new', activitiesCtrl.newActivity)
 
 module.exports = router;
