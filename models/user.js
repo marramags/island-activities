ab2b7998ad4fb2a9cc81c68ac6bfc33ac4c08bfe
marramags <marra.emata@gmail.com>
2022-08-29 13:28:52 -0700

    added userSchema model

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// defining the schema for User model
+const userSchema = new Schema({
+    Name: String,
+    googleId: { 
+        type: String,
+        required: true
+    },
+    email: String,
+    avatar: String
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema); //compiling userSchema into a model & exporting it
