b088550fec8207fecb1994e7333875a3a182ca59
marramags <marra.emata@gmail.com>
2022-08-30 14:54:40 -0700

    added reviews router

--- models/review.js
+++ models/review.js
@@ -3,11 +3,13 @@ const mongood = require('mongoose');
 const Schema = mongoose.Schema;
 
 const reviewsSchema = new Schema ({
-    review: String,
+    reviewContent: String,
     name: String,
-    rating: Number,
-    userID: {type: Schema.Types.ObjectId, ref: 'Activity'},
-    activity: {type: Schema.Types.ObjectId, ref: 'User'},
+    rating: {type: Number, min: 1, max: 5},
+    userId: {type: Schema.Types.ObjectId, ref: 'User'},
+    activityId: {type: Schema.Types.ObjectId, ref: 'Activity'}
+}, {
+    timestamps: true
 })
 
 module.exports = mongoose.model('Review', reviewsSchema);
---
+++ routes/reviews.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var router = express.Router();
+
+// http://localhost:3000/activities/12345/addreview	
+router.post('/activities/:id/')
