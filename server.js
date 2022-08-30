c947e428fcd77a0103714dc74653c2008f3944f5
marramags <marra.emata@gmail.com>
2022-08-30 12:18:54 -0700

    started installing oauth

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session')
+var passport = require('passport');
 var methodOverride = require('method-override');
 require('dotenv').config();
 require('./config/database');
+require('./config/passport');
 
 
 var indexRouter = require('./routes/index');
@@ -22,6 +25,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
